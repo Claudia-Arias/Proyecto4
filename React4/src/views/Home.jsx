4e9f38624170f440287b375ccd40df59ec040455
Maria N <ary023@gmail.com>
2024-02-06 14:40:48 +0100

    routes

---
+++ React4/src/views/Home.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import {Link} from 'react-router-dom'
+
+
+
+
+function Home() {
+  
+
+  return (
+    <>
+    
+     <p>hola soy la pagina Home</p>
+    
+  
+
+
+    </>
+  )
+}
+
+export default Home
---
+++ React4/src/views/Login.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import {Link} from 'react-router-dom'
+import Home from './Home'
+
+
+
+function Login() {
+  
+
+  return (
+    <>
+    
+     <p>hola soy la pagina Login</p>
+    
+    <Link to="/Home">Homeeeeeeee</Link>
+
+
+    </>
+  )
+}
+
+export default Login
