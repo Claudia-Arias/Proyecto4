2f166d5382ae17626f39523f514be016362fa347
GabrielaRamos79 <gabyramos@me.com>
2024-02-07 17:27:22 +0100

    Starting Footer

--- React4/src/components/NavFooter/Footer.jsx
+++ React4/src/components/NavFooter/Footer.jsx
@@ -11,13 +11,29 @@ function Footer() {
         <div className='copyR'>
           
           <p>Copyright &copy; 2024 iMiK Software</p>
+          <p>Aviso legal | Política de privacidad | Términos y condiciones del servicio</p>
+
         </div>
+        
         <div className='textoFooter'>
+
           <p>Contacto: 688 999 777</p>
-          <p>Avda. de la tecnología 123, 29001 Málaga</p>
-          <p>contacto@InstitutoSantaTecla.com</p>
+          <p>Avda. Somos Coders, 29001, Málaga</p>
+          <p>contacto@arrabalschool.com</p>
+
         </div>
-      </div>
+
+        <div className='redesSociales'>
+
+          <img src='./img/facebook.png' alt='Facebook' />
+          <img src='./img/google-mas.png' alt='Google+' />
+          <img src='./img/instagram (1).png' alt="Instagram" />
+          <img src='./img/twiter.png'  alt='Twitter' />
+
+        </div>
+
+        </div>
+      
     </>
   )
 }
