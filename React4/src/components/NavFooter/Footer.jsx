29a6c6e94e7569e2f3edba921200187f4cbe81bb
Maria N <ary023@gmail.com>
2024-02-07 13:18:36 +0100

    test footer

---
+++ React4/src/components/NavFooter/Footer.jsx
@@ -0,0 +1,25 @@
+import './Footer.css'
+
+
+
+function Footer() {
+
+
+  return (
+    <>
+      <div className='footer'>
+        <div className='copyR'>
+          
+          <p>Copyright &copy; 2024 iMiK Software</p>
+        </div>
+        <div className='textoFooter'>
+          <p>Contacto: 688 999 777</p>
+          <p>Avda. de la tecnología 123, 29001 Málaga</p>
+          <p>contacto@InstitutoSantaTecla.com</p>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Footer;
