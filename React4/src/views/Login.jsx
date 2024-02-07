5e65da428a73adba00b7df32207694ae098c9178
claudia <claumarlopez1978@gmail.com>
2024-02-07 12:50:27 +0100

    LoginPage

--- React4/src/views/Login.jsx
+++ React4/src/views/Login.jsx
@@ -1,20 +1,41 @@
 import { useState } from 'react'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import Home from './Home'
+import "./login.css";
 
 
 
 function Login() {
-  
 
+
+const [textoBoton,setTextoBoton] =useState("Login");
+
+  function handleLogin() {
+    setTextoBoton=("hola");
+
+
+  }
   return (
     <>
+   
+      <div className="loginname">
+        <h2>Wheel of Doom</h2>
+        <form>
+          <div className="loginusuario">
+            <label htmlFor="username">Usuario</label>
+            <input type="text" id="username" name="username" required></input>
+          </div>
+          <div className="logincontraseña">
+            <label htmlFor="password">Contraseña</label>
+            <input type="password" id="password" name="password" required></input>
+          </div>
+          <button className= "buttonLogin" text="login" type="button" onClick= {handleLogin}><Link to="/Home">{textoBoton}</Link></button>
+          
+        </form>
+      </div>
     
-     <p>hola soy la pagina Login</p>
-    
-    <Link to="/Home">Homeeeeeeee</Link>
 
-
+     
     </>
   )
 }
