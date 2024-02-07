89300ecb38ea9343aeae4663fcb6f83567324b00
Maria N <ary023@gmail.com>
2024-02-07 13:57:00 +0100

    add Admin List P3

---
+++ React4/src/views/AdminList.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import {Link} from 'react-router-dom'
+
+
+
+
+function AdminList() {
+  
+
+  return (
+    <>
+    
+     <p>hola soy la pagina administracion de listas</p>
+    
+  
+
+
+    </>
+  )
+}
+
+export default AdminList
