7295d0df9dfb065472e1062fe9a93f65712a1f7f
Maria <ary023@gmail.com>
2024-02-07 21:30:25 +0100

    adminList in home was add:only for testing

---
+++ React4/src/components/listAll/ListAll.jsx
@@ -0,0 +1,68 @@
+import './ListAll.css'
+import { useState } from 'react'
+import { UserService } from "../adminList/userService";
+
+
+
+function ListAll() {
+
+    const [userObject, setUserObject] = useState({
+        id: "0",
+        nombre: "",
+        apellido: "",
+        apellido2: "",
+        email: "",
+        telefono: ""
+    });
+
+    const [userListObject, setUserListObject] = useState([]);
+
+    //esta es la función que carga los datos almacenados en el json
+    async function getData() {
+        //accedo a UserService, en concreto a su métod GET. Como este método al ejecutarse retorna los datos de los usuarios,
+        //lo que almaceno en la variable users son esos datos.
+        let users = await UserService.getAllUsers();
+        //ahora actualizo el estado de userList con esta variable (usuarios)
+        setUserListObject(users);
+        setEditable(false);
+    };
+
+    //LLamo a getData() para traer los datos
+    getData();
+
+    return (
+        <>
+            <section className='mostrarDatos'>
+                <section className="seccion_datos">
+                    <table id="tablaDatos" className="tablaDatos" >
+                        <thead className="tablaHead">
+                            <tr>
+                                <th name="celdaName" id="celdaNombre" >Nombre</th>
+                                <th name="celdaApellido1" id="celdaApellido">Apellido 1</th>
+                                <th name="celdaApellido2" id="celdaApellido2">Apellido 2</th>
+                                <th name="celdaRol" id="celdaEmail">Email</th>
+                                <th name="celdaClase" id="celdaTelefono">Teléfono</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                userListObject.map((user, index) => (
+                                    <tr key={index} id={index}>
+                                        <th>{user.nombre}</th>
+                                        <th>{user.apellido}</th>
+                                        <th>{user.apellido2}</th>
+                                        <th>{user.email}</th>
+                                        <th>{user.telefono}</th>
+
+                                    </tr>
+                                ))
+                            }
+                        </tbody>
+                    </table>
+                </section>
+            </section>
+        </>
+    )
+}
+
+export default ListAll
--- React4/src/views/Home.jsx
+++ React4/src/views/Home.jsx
@@ -1,21 +1,30 @@
 import { useState } from 'react'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import PageAdminList from '../components/adminList/PageAdminList'
 import AdminList from './AdminList'
+import ListAll from '../components/listAll/ListAll'
+import './Home.css'
 
 
 
 
 function Home() {
-  
+
 
   return (
     <>
-    
-     {/* <p>hola soy la pagina Home</p> */}
-    
-     <button type="button" ><Link to="/AdminList">Administrar Lista</Link></button>
 
+      {/* <p>hola soy la pagina Home</p> */}
+
+
+      <div className='container'>
+        <div className='containerLeft'>
+          <button type="button" ><Link to="/AdminList">Administrar Lista</Link></button>
+        </div>
+        <div className='containerRigth'>
+          <ListAll />
+        </div>
+      </div>
 
     </>
   )
