5e143b6d5e03d8e51129204e21e879b9a728d485
claudia <claumarlopez1978@gmail.com>
2024-02-08 19:08:14 +0100

    landing page modificado

--- React4/src/views/Login.jsx
+++ React4/src/views/Login.jsx
@@ -11,7 +11,7 @@ function Login() {
 const [textoBoton,setTextoBoton] =useState("Login");
 
   function handleLogin() {
-    setTextoBoton=("hola");
+    setTextoBoton ("hola");
 
 
   }
@@ -22,11 +22,11 @@ const [textoBoton,setTextoBoton] =useState("Login");
         <h2>Wheel of Doom</h2>
         <form>
           <div className="loginusuario">
-            <label htmlFor="username">Usuario</label>
+            <label htmlFor="username">User</label>
             <input type="text" id="username" name="username" required></input>
           </div>
           <div className="logincontraseña">
-            <label htmlFor="password">Contraseña</label>
+            <label htmlFor="password">Password</label>
             <input type="password" id="password" name="password" required></input>
           </div>
           <button className= "buttonLogin" text="login" type="button" onClick= {handleLogin}><Link to="/Home">{textoBoton}</Link></button>
